2b83117fbe2b58d69c406ad1b44f5f3130a17066
Matthew Dahl <dahl.matthew@gmail.com>
2019-03-29 23:54:02 -0400

    Attempt to simplify CI build

---
+++ .bumprrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  ci: {
+    gitUser: {
+      email: 'dogmaiobot@gmail.com',
+      name: 'dogma-io-bot'
+    }
+  },
+  features: {
+    changelog: {enabled: true},
+    timezone: {enabled: true, zone: 'America/New_York'}
+  }
+}
