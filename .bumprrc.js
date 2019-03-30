037f1f38c510dda7b11a7e16f9f1a5177d112033
Matthew Dahl <dahl.matthew@gmail.com>
2019-03-30 16:46:52 -0400

    Update config

--- .bumprrc.js
+++ .bumprrc.js
@@ -8,5 +8,11 @@ module.exports = {
   features: {
     changelog: {enabled: true},
     timezone: {enabled: true, zone: 'America/New_York'}
+  },
+  vcs: {
+    repository: {
+      name: 'skeema',
+      owner: 'dogma-io'
+    }
   }
 }
